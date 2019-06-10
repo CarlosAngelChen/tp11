cddfbb37e99ea58e5ee438ae7d43055e71a92a1a
charlyfigue <50414358+charlyfigue@users.noreply.github.com>
2019-06-10 00:08:23 -0300

    Add files via upload

--- init_raspberry.c
+++ init_raspberry.c
@@ -7,17 +7,39 @@
 
 void init_raspberry(){
 
+char *pin17_export = "/sys/class/gpio/gpio17/value";
+char *pin17_direction = "/sys/class/gpio/gpio17/direction";
+char *pin4_export = "/sys/class/gpio/gpio4/value";
+char *pin4_direction = "/sys/class/gpio/gpio4/direction";
+char *pin18_export = "/sys/class/gpio/gpio18/value";
+char *pin18_direction = "/sys/class/gpio/gpio18/direction";
+char *pin23_export = "/sys/class/gpio/gpio23/value";
+char *pin23_direction = "/sys/class/gpio/gpio23/direction";
+char *pin24_export = "/sys/class/gpio/gpio24/value";
+char *pin24_direction = "/sys/class/gpio/gpio24/direction";
+char *pin25_export = "/sys/class/gpio/gpio25/value";
+char *pin25_direction = "/sys/class/gpio/gpio25/direction";
 char *pin22_export = "/sys/class/gpio/gpio22/value";
 char *pin22_direction = "/sys/class/gpio/gpio22/direction";
+char *pin27_export = "/sys/class/gpio/gpio27/value";
+char *pin27_direction = "/sys/class/gpio/gpio27/direction";
+
+
+char *pines[16] = {pin17_export,pin17_direction, pin4_export,pin4_direction,pin18_export,pin18_direction,
+                    pin23_export,pin23_direction,pin24_export,pin24_direction,pin25_export,pin25_direction,
+                    pin22_export,pin22_direction, pin27_export,pin27_direction};
+char *nums[8] = {"17", "4", "18", "23", "24", "25", "22", "27"};
 
 FILE *handle;
-int nWritten;
-if ((handle =  fopen(pin22_export,"w")) == NULL)
+int nWritten, i;
+
+for(i=0;i<16;i+2){
+if ((handle =  fopen(pines[i],"w")) == NULL)
 {
     printf("Cannot open EXPORT File. Try again later.\n");
     exit(1);
 }
-nWritten = fputs("22",handle);
+nWritten = fputs(nums[i],handle);
 if (nWritten == -1)
 {
     printf("Cannot EXPORT PIN . Try again later.\n");
@@ -28,7 +50,7 @@ else
 
 fclose(handle);
 
-if ((handle = fopen(pin22_direction,"w")) == NULL)
+if ((handle = fopen(pines[i+1],"w")) == NULL)
 {
     printf("Cannot open DIRECTION File");
     exit(1);
@@ -44,5 +66,5 @@ else
     printf("DIRECTION File for PIN opened succesfully\n");
 }
 fclose(handle); // Be carefulldo this for EACH pin !!!
-
+}
 }
