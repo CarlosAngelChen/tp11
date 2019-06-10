ca2f21795430028b18ca7154ecfe8809a32e658b
charlyfigue <50414358+charlyfigue@users.noreply.github.com>
2019-06-09 22:28:46 -0300

    Update raspberry_output.c

--- raspberry_output.c
+++ raspberry_output.c
@@ -20,20 +20,6 @@ if(value_ob)
 else
     obj = '0';
 
-if ((handle =  fopen(pin22,"w")) == NULL)
-{
-    printf("Cannot open EXPORT File. Try again later.\n");
-    exit(1);
-}
-nWritten = fputs("22",handle);
-if (nWritten == -1)
-{
-    printf("Cannot EXPORT PIN . Try again later.\n");
-    exit(1);
-}
-else
-    //printf("EXPORT File opened succesfully\n");
-
 if ((handle = fopen(pin22,"w")) == NULL)
 	{
 		printf("Cannot open device. Try again later.\n");
